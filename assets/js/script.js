7d542ac052c7aa9efda49106b5f6f9aba65eb6e3
HaByeongHyeon <hbh09072001@gmail.com>
2026-08-15 17:11:04 +0900

    pc 메인 페이지 Tablet 반응형 중
    자연스럽게 전환 되도록 수정, pc 버전 가로 스크롤 수정 (mission-sec 으로 추정), 애니메이션 확인, mobile 진행

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,38 +2,54 @@ $(function () {
 
     const $wrap = $(".partner-wrap");
     const $items = $wrap.find("img");
+    const speed = 1;
 
-    // 원본 이미지 세트 복제
     $items.clone().appendTo($wrap);
 
     let x = 0;
-    const speed = 1;
+    const setWidth = $items.toArray().reduce((sum, el) => {
+        return sum + $(el).outerWidth(true);
+    }, 0);
 
-    // 원본 6개 세트의 실제 전체 너비 계산
-    let setWidth = 0;
-
-    $items.each(function () {
-        setWidth += $(this).outerWidth(true);
-    });
-
-    function animate() {
-
+    function move() {
         x -= speed;
 
-        // 원본 세트 하나가 완전히 지나가면
-        // 위치를 같은 거리만큼 되돌려 자연스럽게 반복
         if (Math.abs(x) >= setWidth) {
             x += setWidth;
         }
 
-        $wrap.css(
-            "transform",
-            `translate3d(${x}px, 0, 0)`
-        );
-
-        requestAnimationFrame(animate);
+        $wrap.css("transform", `translateX(${x}px)`);
+        requestAnimationFrame(move);
     }
 
-    animate();
+    move();
+
+});
+
+
+
+
+
+$(function () {
+
+    $(".hamburger-menu").on("click", function () {
+
+        const $menu = $(".mobile-menu");
+        const $icon = $(this).find("img");
+
+        if ($menu.is(":visible")) {
+
+            $menu.stop(true, true).slideUp(400);
+
+        } else {
+
+            $menu
+                .stop(true, true)
+                .hide()
+                .slideDown(400);
+
+        }
+
+    });
 
 });
